c62186baf990a1f48ec42af323bef227d2cce959
Sushil <grayson.9999@gmail.com>
2025-06-06 18:21:28 +1000

    editing UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import ToDo from "./component/ToDo";
+
 import { postNewMessage } from "../helpers/axiosHelper";
 import { useState } from "react";
 function App() {
@@ -28,46 +28,55 @@ function App() {
   return (
     <>
       <h3 className="text-center mt-5">Send Email!</h3>
-      <div className="container form-container d-flex justify-content-center align-items-center mt-5 ">
-        <form className="node-mailer md-8 w-50" onSubmit={handleOnSubmit}>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              name="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="your@email.com"
-              onChange={handleOnChange}
-              value={formData.email}
-            />
+      <div className="container my-5">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 col-lg-6">
+            <form
+              className="p-4 border rounded shadow bg-dark "
+              onSubmit={handleOnSubmit}
+            >
+              <div className="mb-3 ">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="your@email.com"
+                  onChange={handleOnChange}
+                  value={formData.email}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="message" className="form-label">
+                  Message
+                </label>
+                <textarea
+                  className="form-control"
+                  name="message"
+                  placeholder="Leave your message here"
+                  id="message"
+                  rows={4}
+                  value={formData.message}
+                  onChange={handleOnChange}
+                  required
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary w-100 mt-3">
+                Submit
+              </button>
+              {responseMessage && (
+                <div className="alert alert-info mt-3" role="alert">
+                  {responseMessage}
+                </div>
+              )}
+            </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="message" className="message-label">
-              Message
-            </label>
-            <textarea
-              className="form-control"
-              name="message"
-              placeholder="Leave a comment here"
-              id="message"
-              rows={4}
-              value={formData.message}
-              onChange={handleOnChange}
-            ></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary w-100 mt-3">
-            Submit
-          </button>
-          {responseMessage && (
-            <div className="alert alert-info mt-3" role="alert">
-              {responseMessage}
-            </div>
-          )}
-        </form>
+        </div>
       </div>
     </>
   );
